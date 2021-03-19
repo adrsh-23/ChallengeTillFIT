27d42936b02ad6199a8a99c6bd5f225eed7f0a1c
sourav-k7 <neymarsk01@gmail.com>
2021-03-20 03:02:20 +0530

    fixed bottom navbar

--- lib/homePage/homePage.dart
+++ lib/homePage/homePage.dart
@@ -1,66 +1,69 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+  static const List<Widget> _Page = <Widget>[
+    Text('Home Page',
+        style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
+    Text('add Page',
+        style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
+    Text('notification Page',
+        style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
+    Text('Profile Page',
+        style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
+  ];
+  void changePage(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        child: Center(child: Text('homePage')),
+        child: _Page.elementAt(_selectedIndex),
       ),
       bottomNavigationBar: BottomNavigationBar(
-        currentIndex: 0,
         showSelectedLabels: false,
+        onTap: changePage,
+        type: BottomNavigationBarType.shifting,
         showUnselectedLabels: false,
         items: [
           BottomNavigationBarItem(
-            icon: IconButton(
-              icon: Icon(
-                Icons.home,
-                color: Colors.grey,
-                size: 30,
-              ),
-              onPressed: () {
-                //! here add route to home page
-              },
+            icon: Icon(
+              Icons.home,
+              color: Colors.grey,
+              size: 30,
             ),
             label: 'Home',
           ),
           BottomNavigationBarItem(
-            icon: IconButton(
-              icon: Icon(
-                Icons.add,
-                color: Colors.grey,
-                size: 30,
-              ),
-              onPressed: () {
-                //! here add route to the page to add post or challenge other user
-              },
+            icon: Icon(
+              Icons.add,
+              color: Colors.grey,
+              size: 30,
             ),
             label: 'add',
           ),
           BottomNavigationBarItem(
-            icon: IconButton(
-              icon: Icon(
-                Icons.notifications_active_outlined,
-                color: Colors.grey,
-                size: 30,
-              ),
-              onPressed: () {
-                // ! here add route to notification page
-              },
+            icon: Icon(
+              Icons.notifications_active_outlined,
+              color: Colors.grey,
+              size: 30,
             ),
             label: 'notification',
           ),
           BottomNavigationBarItem(
-            icon: IconButton(
-              icon: Icon(
-                Icons.person,
-                color: Colors.grey,
-                size: 30,
-              ),
-              onPressed: () {
-                //!here add route to profile page
-              },
+            icon: Icon(
+              Icons.person,
+              color: Colors.grey,
+              size: 30,
             ),
             label: 'profile',
           ),
