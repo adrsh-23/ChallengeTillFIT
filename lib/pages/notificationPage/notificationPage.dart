5c35d7359dbd67144dcbd1e4306c80a870780344
sourav-k7 <neymarsk01@gmail.com>
2021-03-21 02:42:41 +0530

    improved UI on notification Page

--- lib/pages/notificationPage/notificationPage.dart
+++ lib/pages/notificationPage/notificationPage.dart
@@ -13,6 +13,14 @@ class _NotificationPageState extends State<NotificationPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Notification',
+          style: TextStyle(color: Colors.black),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.tealAccent,
+      ),
       body: StreamBuilder(
           stream: usersCollection
               .doc(FirebaseAuth.instance.currentUser.uid)
@@ -26,43 +34,54 @@ class _NotificationPageState extends State<NotificationPage> {
             return ListView.builder(
               itemBuilder: (context, index) {
                 DocumentSnapshot challengeDoc = snapshots.data.docs[index];
-                return ListTile(
-                  leading: CircleAvatar(
-                    backgroundImage: NetworkImage(challengeDoc['profilePic']),
-                  ),
-                  title: Text(
-                    challengeDoc['challengerName'],
-                    style: TextStyle(fontSize: 20, color: Colors.white),
-                  ),
-                  subtitle: Column(
-                    children: [
-                      Text(
-                        challengeDoc['challenge'],
-                        style: TextStyle(fontSize: 25, color: Colors.white),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          Text(
-                            tAgo
-                                .format(challengeDoc['timeStamp'].toDate())
-                                .toString(),
-                            style: TextStyle(color: Colors.white),
+                return Card(
+                  color: Colors.transparent,
+                  child: ListTile(
+                    tileColor: Colors.tealAccent.withOpacity(0.5),
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage(challengeDoc['profilePic']),
+                    ),
+                    title: Text(
+                      challengeDoc['challenge'],
+                      style: TextStyle(fontSize: 25, color: Colors.white),
+                    ),
+                    subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'By ${challengeDoc['challengerName']}',
+                          style:
+                              TextStyle(fontSize: 18, color: Colors.grey[400]),
+                        ),
+                        Text(
+                          tAgo
+                              .format(challengeDoc['timeStamp'].toDate())
+                              .toString(),
+                          style: TextStyle(
+                            color: Colors.grey[400],
+                            fontSize: 15,
                           ),
-                        ],
+                        ),
+                      ],
+                    ),
+                    trailing: MaterialButton(
+                      color: Color.fromRGBO(0, 16, 48, 1),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20)),
+                      onPressed: () async {
+                        await FirebaseFirestore.instance
+                            .runTransaction((Transaction myTransaction) async {
+                          myTransaction
+                              .delete(snapshots.data.docs[index].reference);
+                        });
+                      },
+                      child: Text(
+                        "Decline",
+                        style: TextStyle(
+                          color: Colors.tealAccent,
+                        ),
                       ),
-                      MaterialButton(
-                        color: Colors.white,
-                        onPressed: () async {
-                          await FirebaseFirestore.instance.runTransaction(
-                              (Transaction myTransaction) async {
-                            myTransaction
-                                .delete(snapshots.data.docs[index].reference);
-                          });
-                        },
-                        child: Text("Decline"),
-                      ),
-                    ],
+                    ),
                   ),
                 );
               },
